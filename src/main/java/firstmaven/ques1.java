9ee680d9a805278518f64fdc1eec0c02e27f8c4f
ttn <ttn@TTNPL>
2021-02-26 00:19:30 +0530

    New commit for maven.

---
+++ src/main/java/firstmaven/ques1.java
@@ -0,0 +1,11 @@
+package firstmaven;
+
+public class ques1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world...");
+
+	}
+
+}
---
+++ src/test/java/firstmaven/test1.java
@@ -0,0 +1,14 @@
+package firstmaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
